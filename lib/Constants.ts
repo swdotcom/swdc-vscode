f76d40bf5b32f36d848dbdf6c97ef3e4353e83a6
Ofer <ofer@software.com>
2019-03-13 10:42:11 -0700

    Adding the 'qa' endpoints to the comments.

--- lib/Constants.ts
+++ lib/Constants.ts
@@ -15,12 +15,10 @@ export const SHORT_THRESHOLD_HOURS = 1;
 export const PLUGIN_ID = 2;
 
 // set the api endpoint to use
-// lksdjflsjklkjsdf
-// lkjdsflkjsdflkjsdfklj
-// "http://localhost:5000", "https://stagingapi.software.com", "https://api.software.com"
+// "http://localhost:5000", "https://qaapi.software.com", "https://stagingapi.software.com", "https://api.software.com"
 export const api_endpoint = "https://api.software.com";
 // set the launch url to use
-// "http://localhost:3000", "https://staging.software.com", "https://app.software.com"
+// "http://localhost:3000", "https://qa.software.com", "https://staging.software.com", "https://app.software.com"
 export const launch_url = "https://app.software.com";
 
 // export const dashboard_uri = Uri.parse("swdc://Software/SoftwareDashboard");
