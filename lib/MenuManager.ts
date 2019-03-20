07af891b60be61468b90ffe83fe213f8f56f3c11
Ofer <ofer@software.com>
2019-03-20 08:42:05 -0700

    SFTW-1415: Update web dashboard link from "/" to "/login"

--- lib/MenuManager.ts
+++ lib/MenuManager.ts
@@ -14,7 +14,7 @@ let showMusicMetrics = false;
  * fetch the show music metrics flag
  */
 export function updateShowMusicMetrics(val) {
-    showMusicMetrics = val;
+  showMusicMetrics = val;
 }
 
 /**
@@ -23,141 +23,137 @@ export function updateShowMusicMetrics(val) {
  */
 
 export function showQuickPick(pickOptions) {
-    if (!pickOptions || !pickOptions["items"]) {
-        return;
-    }
-    let options: QuickPickOptions = {
-        onDidSelectItem: item => {
-            window.setStatusBarMessage(item["label"]);
-        },
-        matchOnDescription: false,
-        matchOnDetail: false,
-        placeHolder: pickOptions.placeholder || ""
-    };
-    window.showQuickPick(pickOptions.items, options).then(async item => {
-        if (item) {
-            let url = item["url"];
-            let uri = item["uri"];
-            let cb = item["cb"];
-            if (url) {
-                launchWebUrl(url);
-                if (url.includes("?")) {
-                    refetchUserStatusLazily();
-                }
-            } else if (uri) {
-                displayCodeTimeMetricsDashboard();
-            }
-            if (cb) {
-                cb();
-            }
+  if (!pickOptions || !pickOptions["items"]) {
+    return;
+  }
+  let options: QuickPickOptions = {
+    onDidSelectItem: item => {
+      window.setStatusBarMessage(item["label"]);
+    },
+    matchOnDescription: false,
+    matchOnDetail: false,
+    placeHolder: pickOptions.placeholder || ""
+  };
+  window.showQuickPick(pickOptions.items, options).then(async item => {
+    if (item) {
+      let url = item["url"];
+      let uri = item["uri"];
+      let cb = item["cb"];
+      if (url) {
+        launchWebUrl(url);
+        if (url.includes("?")) {
+          refetchUserStatusLazily();
         }
-    });
+      } else if (uri) {
+        displayCodeTimeMetricsDashboard();
+      }
+      if (cb) {
+        cb();
+      }
+    }
+  });
 }
 
 export async function buildLoginUrl() {
-    let jwt = getItem("jwt");
-    let encodedJwt = encodeURIComponent(jwt);
-    let loginUrl = `${launch_url}/onboarding?token=${encodedJwt}`;
-    return loginUrl;
+  let jwt = getItem("jwt");
+  let encodedJwt = encodeURIComponent(jwt);
+  let loginUrl = `${launch_url}/onboarding?token=${encodedJwt}`;
+  return loginUrl;
 }
 
 export async function buildWebDashboardUrl() {
-    let webUrl = launch_url;
-    return webUrl;
+  let webUrl = launch_url;
+  return webUrl;
 }
 
 export async function showMenuOptions() {
-    let filePath = getDashboardFile();
-    // {loggedIn: true|false}
-    let userStatus = await getUserStatus();
-    let webUrl = await buildWebDashboardUrl();
-    let loginUrl = await buildLoginUrl();
+  let filePath = getDashboardFile();
+  // {loggedIn: true|false}
+  let userStatus = await getUserStatus();
+  let webUrl = await buildWebDashboardUrl();
+  let loginUrl = await buildLoginUrl();
 
-    // {placeholder, items: [{label, description, url, details, tooltip},...]}
-    let kpmMenuOptions = {
-        items: []
-    };
+  // {placeholder, items: [{label, description, url, details, tooltip},...]}
+  let kpmMenuOptions = {
+    items: []
+  };
 
+  kpmMenuOptions.items.push({
+    label: "Code time dashboard",
+    description: "",
+    detail: "View your latest coding metrics right here in your editor",
+    url: null,
+    uri: filePath,
+    cb: null
+  });
+
+  if (userStatus.loggedIn && showMusicMetrics) {
     kpmMenuOptions.items.push({
-        label: "Code time dashboard",
-        description: "",
-        detail: "View your latest coding metrics right here in your editor",
-        url: null,
-        uri: filePath,
-        cb: null
+      label: "Software Top 40",
+      description: "",
+      detail:
+        "Top 40 most popular songs developers around the world listen to as they code",
+      url: "https://api.software.com/music/top40",
+      uri: null,
+      cb: null
     });
-
-    if (userStatus.loggedIn && showMusicMetrics) {
-        kpmMenuOptions.items.push({
-            label: "Software Top 40",
-            description: "",
-            detail:
-                "Top 40 most popular songs developers around the world listen to as they code",
-            url: "https://api.software.com/music/top40",
-            uri: null,
-            cb: null
-        });
-    }
-    if (!userStatus.loggedIn) {
-        kpmMenuOptions.items.push({
-            label: LOGIN_LABEL,
-            description: "",
-            detail:
-                "To see your coding data in Code Time, please log in to your account",
-            url: loginUrl,
-            uri: null,
-            cb: null
-        });
-    } else {
-        kpmMenuOptions.items.push({
-            label: "Web dashboard",
-            description: "",
-            detail: "See rich data visualizations in the web app",
-            url: webUrl,
-            uri: null,
-            cb: null
-        });
-    }
-    showQuickPick(kpmMenuOptions);
+  }
+  if (!userStatus.loggedIn) {
+    kpmMenuOptions.items.push({
+      label: LOGIN_LABEL,
+      description: "",
+      detail:
+        "To see your coding data in Code Time, please log in to your account",
+      url: loginUrl,
+      uri: null,
+      cb: null
+    });
+  } else {
+    kpmMenuOptions.items.push({
+      label: "Web dashboard",
+      description: "",
+      detail: "See rich data visualizations in the web app",
+      url: webUrl + "/login",
+      uri: null,
+      cb: null
+    });
+  }
+  showQuickPick(kpmMenuOptions);
 }
 
 export async function fetchCodeTimeMetricsDashboard() {
-    let filePath = getDashboardFile();
+  let filePath = getDashboardFile();
 
-    let showMusicMetrics = workspace.getConfiguration().get("showMusicMetrics");
-    let showGitMetrics = workspace.getConfiguration().get("showGitMetrics");
-    let showWeeklyRanking = workspace
-        .getConfiguration()
-        .get("showWeeklyRanking");
+  let showMusicMetrics = workspace.getConfiguration().get("showMusicMetrics");
+  let showGitMetrics = workspace.getConfiguration().get("showGitMetrics");
+  let showWeeklyRanking = workspace.getConfiguration().get("showWeeklyRanking");
 
-    const dashboardSummary = await softwareGet(
-        `/dashboard?showMusic=${showMusicMetrics}&showGit=${showGitMetrics}&showRank=${showWeeklyRanking}&linux=${isLinux()}`,
-        getItem("jwt")
-    );
-    // get the content
-    let content =
-        dashboardSummary && dashboardSummary.data
-            ? dashboardSummary.data
-            : NO_DATA;
+  const dashboardSummary = await softwareGet(
+    `/dashboard?showMusic=${showMusicMetrics}&showGit=${showGitMetrics}&showRank=${showWeeklyRanking}&linux=${isLinux()}`,
+    getItem("jwt")
+  );
+  // get the content
+  let content =
+    dashboardSummary && dashboardSummary.data ? dashboardSummary.data : NO_DATA;
 
-    fs.writeFileSync(filePath, content, err => {
-        if (err) {
-            console.log(
-                "Code Time: Error writing to the Software session file: ",
-                err.message
-            );
-        }
-    });
+  fs.writeFileSync(filePath, content, err => {
+    if (err) {
+      console.log(
+        "Code Time: Error writing to the Software session file: ",
+        err.message
+      );
+    }
+  });
 }
 
 export async function displayCodeTimeMetricsDashboard() {
-    let filePath = getDashboardFile();
-    await fetchCodeTimeMetricsDashboard();
+  let filePath = getDashboardFile();
+  await fetchCodeTimeMetricsDashboard();
 
-    workspace.openTextDocument(filePath).then(doc => {
-        // only focus if it's not already open
-        window.showTextDocument(doc, ViewColumn.One, false).then(e => {
-            // done
-        });
+  workspace.openTextDocument(filePath).then(doc => {
+    // only focus if it's not already open
+    window.showTextDocument(doc, ViewColumn.One, false).then(e => {
+      // done
     });
+  });
 }
