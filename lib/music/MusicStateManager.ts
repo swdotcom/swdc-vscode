af16c0303ffa5b6ca6dead000395e1f36e198f7a
Xavier <xavluiz@gmail.com>
2019-05-24 06:29:06 -0700

    fixed updating when a track toggles

--- lib/music/MusicStateManager.ts
+++ lib/music/MusicStateManager.ts
@@ -113,79 +113,113 @@ export class MusicStateManager {
             await getSpotifyOauth(serverIsOnline);
         }
 
-        const playingTrack = await CodyMusic.getRunningTrack();
+        let playingTrack = await CodyMusic.getRunningTrack();
 
-        if (playingTrack && playingTrack.id) {
-            playingTrack["start"] = 0;
-            playingTrack["end"] = 0;
+        if (!playingTrack) {
+            playingTrack = new CodyMusic.Track();
+        }
 
-            let d = new Date();
-            // offset is the minutes from GMT. it's positive if it's before, and negative after
-            const offset = d.getTimezoneOffset();
-            const offset_sec = offset * 60;
-            let nowInSec: number = Math.round(d.getTime() / 1000);
-            // subtract the offset_sec (it'll be positive before utc and negative after utc)
-            let localNowInSec = nowInSec - offset_sec;
-            let state = "stopped";
-            let playingTrackId = playingTrack["id"] || null;
-            if (
-                playingTrack["state"] !== undefined &&
-                playingTrack["state"] !== null
-            ) {
-                state = playingTrack["state"];
-            }
+        playingTrack["start"] = 0;
+        playingTrack["end"] = 0;
 
-            let isPaused =
-                state.toLowerCase().indexOf("playing") !== -1 ? false : true;
+        let d = new Date();
+        // offset is the minutes from GMT. it's positive if it's before, and negative after
+        const offset = d.getTimezoneOffset();
+        const offset_sec = offset * 60;
+        let nowInSec: number = Math.round(d.getTime() / 1000);
+        // subtract the offset_sec (it'll be positive before utc and negative after utc)
+        let localNowInSec = nowInSec - offset_sec;
+        let state = "stopped";
+        let playingTrackId = playingTrack["id"] || null;
+        if (
+            playingTrack["state"] !== undefined &&
+            playingTrack["state"] !== null
+        ) {
+            state = playingTrack["state"];
+        }
 
-            let existingTrackId = this.existingTrack["id"] || null;
-            let playingTrackDuration: number = null;
-            if (playingTrack.duration && playingTrack.duration > 0) {
-                playingTrackDuration = playingTrack.duration;
-            } else if (
-                playingTrack.duration_ms &&
-                playingTrack.duration_ms > 0
-            ) {
-                playingTrackDuration = playingTrack.duration_ms;
-            }
+        let isPaused =
+            state.toLowerCase().indexOf("playing") !== -1 ? false : true;
 
-            if (!playingTrackId && existingTrackId) {
-                // we don't have a track playing and we have an existing one, close it out
-                this.existingTrack["end"] = nowInSec;
+        let existingTrackId = this.existingTrack["id"] || null;
+        let playingTrackDuration: number = null;
+        if (playingTrack.duration && playingTrack.duration > 0) {
+            playingTrackDuration = playingTrack.duration;
+        } else if (playingTrack.duration_ms && playingTrack.duration_ms > 0) {
+            playingTrackDuration = playingTrack.duration_ms;
+        }
+
+        if (!playingTrackId && existingTrackId) {
+            // we don't have a track playing and we have an existing one, close it out
+            this.existingTrack["end"] = nowInSec;
+
+            hasChanges = true;
+
+            // send the existing to close it out
+            sendMusicData(this.existingTrack).then(result => {
+                // clear out the trackInfo
+                this.existingTrack = {};
+                this.lastTimeSent = null;
+            });
+        } else if (playingTrackId && !existingTrackId) {
+            // first check if it needs to be synced in regard to the loved state
+            await this.updateLovedStateFromServer(playingTrack);
+
+            // this means we don't have an existing track, the playing track will be our new existing track
+            // it doesn't matter if it's paused or not since we don't have an existing track
+            this.existingTrack = {};
+            this.existingTrack = { ...playingTrack };
+            this.existingTrack["start"] = nowInSec;
+            this.existingTrack["local_start"] = localNowInSec;
+
+            hasChanges = true;
+
+            // send the existing (which is the initial one for this session)
+            sendMusicData(this.existingTrack);
+            this.lastTimeSent = nowInSec;
+        } else if (playingTrackId && existingTrackId) {
+            // we have a playing track and an existing track, are they the same ones?
+            if (playingTrackId !== existingTrackId) {
+                // send the existing song now to close it out
+                this.existingTrack["end"] = nowInSec - 1;
 
                 hasChanges = true;
 
-                // send the existing to close it out
                 sendMusicData(this.existingTrack).then(result => {
                     // clear out the trackInfo
                     this.existingTrack = {};
-                    this.lastTimeSent = null;
+                    // start the new song
+                    this.existingTrack = { ...playingTrack };
+                    this.existingTrack["start"] = nowInSec;
+                    this.existingTrack["local_start"] = localNowInSec;
+                    sendMusicData(this.existingTrack);
+                    this.lastTimeSent = nowInSec;
                 });
-            } else if (playingTrackId && !existingTrackId) {
-                // first check if it needs to be synced in regard to the loved state
-                await this.updateLovedStateFromServer(playingTrack);
-
-                // this means we don't have an existing track, the playing track will be our new existing track
-                // it doesn't matter if it's paused or not since we don't have an existing track
-                this.existingTrack = {};
-                this.existingTrack = { ...playingTrack };
-                this.existingTrack["start"] = nowInSec;
-                this.existingTrack["local_start"] = localNowInSec;
-
-                hasChanges = true;
-
-                // send the existing (which is the initial one for this session)
-                sendMusicData(this.existingTrack);
-                this.lastTimeSent = nowInSec;
-            } else if (playingTrackId && existingTrackId) {
-                // we have a playing track and an existing track, are they the same ones?
-                if (playingTrackId !== existingTrackId) {
-                    // send the existing song now to close it out
+            } else {
+                // it's the same trackId, but we may need to send it again
+                // if the song is on repeat. the only way to find out is to check
+                // if it's not paused and the last time we sent this is longer than
+                // the duration.
+                // check if it's not paused and is beyond the track duration
+                let diffInSec: number = this.lastTimeSent
+                    ? nowInSec - this.lastTimeSent
+                    : 0;
+                if (
+                    !isPaused &&
+                    playingTrackDuration &&
+                    this.lastTimeSent &&
+                    diffInSec > playingTrackDuration
+                ) {
+                    // it's on repeat, send it and start the next one
                     this.existingTrack["end"] = nowInSec - 1;
 
                     hasChanges = true;
 
-                    sendMusicData(this.existingTrack).then(result => {
+                    // close it out
+                    sendMusicData(this.existingTrack).then(async result => {
+                        // first check if it needs to be synced in regard to the loved state
+                        await this.updateLovedStateFromServer(playingTrack);
+
                         // clear out the trackInfo
                         this.existingTrack = {};
                         // start the new song
@@ -195,41 +229,6 @@ export class MusicStateManager {
                         sendMusicData(this.existingTrack);
                         this.lastTimeSent = nowInSec;
                     });
-                } else {
-                    // it's the same trackId, but we may need to send it again
-                    // if the song is on repeat. the only way to find out is to check
-                    // if it's not paused and the last time we sent this is longer than
-                    // the duration.
-                    // check if it's not paused and is beyond the track duration
-                    let diffInSec: number = this.lastTimeSent
-                        ? nowInSec - this.lastTimeSent
-                        : 0;
-                    if (
-                        !isPaused &&
-                        playingTrackDuration &&
-                        this.lastTimeSent &&
-                        diffInSec > playingTrackDuration
-                    ) {
-                        // it's on repeat, send it and start the next one
-                        this.existingTrack["end"] = nowInSec - 1;
-
-                        hasChanges = true;
-
-                        // close it out
-                        sendMusicData(this.existingTrack).then(async result => {
-                            // first check if it needs to be synced in regard to the loved state
-                            await this.updateLovedStateFromServer(playingTrack);
-
-                            // clear out the trackInfo
-                            this.existingTrack = {};
-                            // start the new song
-                            this.existingTrack = { ...playingTrack };
-                            this.existingTrack["start"] = nowInSec;
-                            this.existingTrack["local_start"] = localNowInSec;
-                            sendMusicData(this.existingTrack);
-                            this.lastTimeSent = nowInSec;
-                        });
-                    }
                 }
             }
         }
