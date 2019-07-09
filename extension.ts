16c5ec078727b641ffae96cea3ad7f14749266c3
Xavier <xavluiz@gmail.com>
2019-07-09 15:02:35 -0700

    updated batch send timer

--- extension.ts
+++ extension.ts
@@ -191,7 +191,7 @@ export async function intializePlugin(
         const half_hour_ms = hourly_interval_ms / 2;
         offline_data_interval = setInterval(() => {
             sendOfflineData();
-        }, one_min_ms * 2);
+        }, half_hour_ms);
 
         // in 2 minutes fetch the historical commits if any
         setTimeout(() => {
