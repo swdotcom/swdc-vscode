7f1c0d40b3f865146757778bd79e881bc1885867
brettmstevens7 <brettmstevens7@gmail.com>
2020-04-08 08:42:28 -0700

    simplify getNowTimes() function

--- lib/Util.ts
+++ lib/Util.ts
@@ -716,26 +716,35 @@ export function getOffsetSeconds() {
 }
 
 /**
- * Return the local and utc unix and day values
+ * now - current time in UTC (Moment object)
+ * now_in_sec - current time in UTC, unix seconds
+ * offset_in_sec - timezone offset from UTC (sign = -420 for Pacific Time)
+ * local_now_in_sec - current time in UTC plus the timezone offset
+ * utcDay - current day in UTC
+ * day - current day in local TZ
+ * localDayTime - current day in local TZ
+ *
+ * Example:
+ * { day: "2020-04-07", localDayTime: "Tuesday, April 7, 2020 9:48 PM",
+ * local_now_in_sec: 1586296107, now: "2020-04-08T04:48:27.120Z", now_in_sec: 1586321307,
+ * offset_in_sec: -25200, utcDay: "2020-04-08" }
  */
 export function getNowTimes() {
-    const now_in_sec = moment().unix();
-    const local = moment().local();
-    const offset_in_sec = moment.parseZone(local).utcOffset() * 60;
+    const now = moment.utc();
+    const now_in_sec = now.unix();
+    const offset_in_sec = moment().utcOffset() * 60;
     const local_now_in_sec = now_in_sec + offset_in_sec;
-    const day = moment()
-        .utcOffset(moment.parseZone(local).utcOffset())
-        .format(dayFormat);
-    const utcDay = moment().utcOffset(0).format(dayTimeFormat);
-    const localDayTime = moment()
-        .utcOffset(moment.parseZone(local).utcOffset())
-        .format(dayTimeFormat);
+    const utcDay = now.format(dayFormat);
+    const day = moment().format(dayFormat);
+    const localDayTime = moment().format(dayTimeFormat);
 
     return {
+        now,
         now_in_sec,
+        offset_in_sec,
         local_now_in_sec,
-        day,
         utcDay,
+        day,
         localDayTime,
     };
 }
