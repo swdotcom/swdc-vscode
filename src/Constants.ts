4307ee654c6649a30b64aba6c00a560065553234
Xavier Luiz <xavluiz@gmail.com>
2021-02-07 22:46:30 -0800

    reverted software directory

--- src/Constants.ts
+++ src/Constants.ts
@@ -13,7 +13,7 @@ export const SIGN_UP_LABEL = "Sign up";
 export const DISCONNECT_LABEL = "Disconnect";
 
 // Directory where software data is stored
-export const SOFTWARE_DIRECTORY = ".software-webprovider"; //".software";
+export const SOFTWARE_DIRECTORY = ".software";
 
 // API ENDPOINT
 export const api_endpoint = "https://api.software.com";
