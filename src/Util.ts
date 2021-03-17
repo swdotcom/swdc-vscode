4d6fa89669101912712a8e277f2763d3f03bff47
Xavier Luiz <xavluiz@gmail.com>
2021-03-17 12:54:21 -0700

    code cleanup and use jwt if no user or integration fetch

--- src/Util.ts
+++ src/Util.ts
@@ -614,13 +614,6 @@ export function randomCode() {
     .toString();
 }
 
-export function deleteFile(file) {
-  // if the file exists, get it
-  if (fs.existsSync(file)) {
-    fs.unlinkSync(file);
-  }
-}
-
 function execPromise(command, opts) {
   return new Promise(function (resolve, reject) {
     exec(command, opts, (error, stdout, stderr) => {
@@ -705,15 +698,6 @@ function checkRegistration() {
     return false;
   }
   return true;
-}
-
-/**
- * @param num The number to round
- * @param precision The number of decimal places to preserve
- */
-function roundUp(num, precision) {
-  precision = Math.pow(10, precision);
-  return Math.ceil(num * precision) / precision;
 }
 
 export function formatNumber(num) {
@@ -852,109 +836,6 @@ export function showInformationMessage(message: string) {
 
 export function showWarningMessage(message: string) {
   return window.showWarningMessage(`${message}`);
-}
-
-function formatRightAlignedTableLabel(label, col_width) {
-  const spacesRequired = col_width - label.length;
-  let spaces = "";
-  if (spacesRequired > 0) {
-    for (let i = 0; i < spacesRequired; i++) {
-      spaces += " ";
-    }
-  }
-  return `${spaces}${label}`;
-}
-
-export function getTableHeader(leftLabel, rightLabel, isFullTable = true) {
-  // get the space between the two labels
-  const fullLen = !isFullTable ? TABLE_WIDTH - DASHBOARD_COL_WIDTH : TABLE_WIDTH;
-  const spacesRequired = fullLen - leftLabel.length - rightLabel.length;
-  let spaces = "";
-  if (spacesRequired > 0) {
-    let str = "";
-    for (let i = 0; i < spacesRequired; i++) {
-      spaces += " ";
-    }
-  }
-  return `${leftLabel}${spaces}${rightLabel}`;
-}
-
-export function getRightAlignedTableHeader(label) {
-  let content = `${formatRightAlignedTableLabel(label, TABLE_WIDTH)}\n`;
-  for (let i = 0; i < TABLE_WIDTH; i++) {
-    content += "-";
-  }
-  content += "\n";
-  return content;
-}
-
-function getSpaces(spacesRequired) {
-  let spaces = "";
-  if (spacesRequired > 0) {
-    let str = "";
-    for (let i = 0; i < spacesRequired; i++) {
-      spaces += " ";
-    }
-  }
-  return spaces;
-}
-
-export function getRowLabels(labels) {
-  // for now 3 columns
-  let content = "";
-  let spacesRequired = 0;
-  for (let i = 0; i < labels.length; i++) {
-    const label = labels[i];
-    if (i === 0) {
-      content += label;
-      // show a colon at the end of this column
-      spacesRequired = DASHBOARD_COL_WIDTH - content.length - 1;
-      content += getSpaces(spacesRequired);
-      content += ":";
-    } else if (i === 1) {
-      // middle column
-      spacesRequired = DASHBOARD_LRG_COL_WIDTH + DASHBOARD_COL_WIDTH - content.length - label.length - 1;
-      content += getSpaces(spacesRequired);
-      content += `${label} `;
-    } else {
-      // last column, get spaces until the end
-      spacesRequired = DASHBOARD_COL_WIDTH - label.length - 2;
-      content += `| `;
-      content += getSpaces(spacesRequired);
-      content += label;
-    }
-  }
-  content += "\n";
-  return content;
-}
-
-export function getColumnHeaders(labels) {
-  // for now 3 columns
-  let content = "";
-  let spacesRequired = 0;
-  for (let i = 0; i < labels.length; i++) {
-    const label = labels[i];
-    if (i === 0) {
-      content += label;
-    } else if (i === 1) {
-      // middle column
-      spacesRequired = DASHBOARD_LRG_COL_WIDTH + DASHBOARD_COL_WIDTH - content.length - label.length - 1;
-      content += getSpaces(spacesRequired);
-      content += `${label} `;
-    } else {
-      // last column, get spaces until the end
-      spacesRequired = DASHBOARD_COL_WIDTH - label.length - 2;
-      content += `| `;
-      content += getSpaces(spacesRequired);
-      content += label;
-    }
-  }
-  content += "\n";
-  for (let i = 0; i < TABLE_WIDTH; i++) {
-    content += "-";
-  }
-  content += "\n";
-  return content;
 }
 
 export function getFileType(fileName: string) {
