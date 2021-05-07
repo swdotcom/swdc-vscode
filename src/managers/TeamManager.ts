8e96d17c3ecd7a0600e2242ead92f333601d589a
Xavier Luiz <xavluiz@gmail.com>
2021-05-07 07:00:56 -0700

    update team links based on rails app

--- src/managers/TeamManager.ts
+++ src/managers/TeamManager.ts
@@ -2,21 +2,32 @@ import { isResponseOk, softwareGet } from "../http/HttpClient";
 import { getItem } from "../Util";
 
 let initializedCache = false;
-let cachedTeams = [];
+let teams = [];
 
 export async function getTeams() {
   initializedCache = true;
-  cachedTeams = [];
-  const resp = await softwareGet("/teams", getItem("jwt"));
+  teams = [];
+  const resp = await softwareGet("/v1/organizations", getItem("jwt"));
   if (isResponseOk(resp)) {
-    cachedTeams = resp.data;
+    const orgs = resp.data;
+    if (orgs?.length) {
+      orgs.forEach((org) => {
+        org.teams.forEach((team) => {
+          teams.push({
+            ...team,
+            org_name: org.name,
+            org_id: org.id,
+          });
+        });
+      });
+    }
   }
-  return cachedTeams;
+  return teams;
 }
 
 export async function getCachedTeams() {
   if (!initializedCache) {
     return getTeams();
   }
-  return cachedTeams;
+  return teams;
 }
