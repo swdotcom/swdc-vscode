491339a99968c09fa880ca578b50b9a848394af8
Xavier Luiz <xavluiz@gmail.com>
2021-05-17 10:20:14 -0700

    add user_id null check

--- src/message_handlers/current_day_stats_update.ts
+++ src/message_handlers/current_day_stats_update.ts
@@ -2,6 +2,8 @@ import { SummaryManager } from "../managers/SummaryManager";
 
 // { user_id: row["USER_ID"], data: SessionSummary, action: "update" }
 export async function handleCurrentDayStatsUpdate(currentDayStatsInfo) {
-  // update the session summary data
-  SummaryManager.getInstance().updateCurrentDayStats(currentDayStatsInfo.data);
+  if (currentDayStatsInfo?.user_id) {
+    // update the session summary data
+    SummaryManager.getInstance().updateCurrentDayStats(currentDayStatsInfo.data);
+  }
 }
