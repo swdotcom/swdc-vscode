1707e109f1e7014e9c0103072c0d2d5239a11afb
Xavier Luiz <xavluiz@gmail.com>
2021-06-11 05:11:50 -0700

    updated the logic to make it more readable

--- src/notifications/endOfDay.ts
+++ src/notifications/endOfDay.ts
@@ -24,46 +24,37 @@ export const setEndOfDayNotification = async (user: any) => {
 
     const d = new Date();
     const day = format(d, "EEE").toLowerCase();
-    if (jwt) {
-      // get the user's work hours from their profile
-      const response = await softwareGet("/users/profile", jwt);
-      let msUntilEndOfTheDay = 0;
-      if (isResponseOk(response) && response.data?.work_hours) {
-        // check if this day is active
-        const work_hours_today = response.data.work_hours[day] || undefined;
-        if (!work_hours_today) {
-          // in case it's not in the correct i.e {'tue': {ranges: [...]}} format, set it to 5pm
-          if (day !== "sun" && day !== "sat") {
-            msUntilEndOfTheDay = getMillisUntilEndOfTheDay(d, HOUR_IN_MILLIS * 17);
-          }
-        } else if (work_hours_today.active) {
-          // it's active, get the largest end range
-          const endTimes = work_hours_today.ranges.map((n) => {
-            // convert "end" to total seconds in a day
-            return getEndTimeSeconds(n.end);
-          });
+    let msUntilEndOfTheDay = 0;
 
-          // sort milliseconds in descending order
-          endTimes.sort(function (a, b) {
-            return b - a;
-          });
+    // [[118800,147600],[205200,234000],[291600,320400],[378000,406800],[464400,493200]]
+    // default of 5pm if the response or work_hours format doesn't have the {dow:...}
+    if (day !== "sun" && day !== "sat") {
+      msUntilEndOfTheDay = getMillisUntilEndOfTheDay(d, HOUR_IN_MILLIS * 17);
+    }
 
-          msUntilEndOfTheDay = getMillisUntilEndOfTheDay(d, endTimes[0]);
-        }
-      } else {
-        console.error("[CodeTime] error response from /users/profile", response);
-        // the work hours may come in this format as well
-        // [[118800,147600],[205200,234000],[291600,320400],[378000,406800],[464400,493200]]
-        // just give a default of 5pm
-        if (day !== "sun" && day !== "sat") {
-          msUntilEndOfTheDay = getMillisUntilEndOfTheDay(d, HOUR_IN_MILLIS * 17);
-        }
+    const response = await softwareGet("/users/profile", jwt);
+    if (isResponseOk(response)) {
+      // get the day of the week that matches today
+      const work_hours_today = response.data.work_hours[day] || undefined;
+      if (work_hours_today?.active) {
+        // it's active, get the largest end range
+        const endTimes = work_hours_today.ranges.map((n) => {
+          // convert "end" to total seconds in a day
+          return getEndTimeSeconds(n.end);
+        });
+
+        // sort milliseconds in descending order
+        endTimes.sort(function (a, b) {
+          return b - a;
+        });
+
+        msUntilEndOfTheDay = getMillisUntilEndOfTheDay(d, endTimes[0]);
       }
+    }
 
-      if (msUntilEndOfTheDay > 0) {
-        // set the timer to fire in "n" number of milliseconds
-        timer = setTimeout(showEndOfDayNotification, msUntilEndOfTheDay);
-      }
+    if (msUntilEndOfTheDay > 0) {
+      // set the timer to fire in "n" milliseconds
+      timer = setTimeout(showEndOfDayNotification, msUntilEndOfTheDay);
     }
   }
 };
