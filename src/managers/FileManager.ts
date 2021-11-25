18654c2695b54780cffbbc6708951f151ec9f796
Xavier Luiz <35306917+xavluiz@users.noreply.github.com>
2021-11-24 16:17:01 -0800

    remove file-it

--- src/managers/FileManager.ts
+++ src/managers/FileManager.ts
@@ -1,35 +1,36 @@
-const fileIt = require('file-it');
+import { logIt } from '../Util';
 
-// Synchronous file handling methods via "file-it"
+const fs = require('fs');
 
-export function storeJsonData(file: string, data: any) {
-  fileIt.writeJsonFileSync(file, data);
-}
-
-export function storeContentData(file: string, content: any) {
-  fileIt.writeContentFileSync(file, content);
+export function getJsonItem(file: string, key: string) {
+  const data: any = getFileDataAsJson(file);
+  return data ? data[key] : null;
 }
 
 export function setJsonItem(file: string, key: string, value: any) {
-  fileIt.setJsonValue(file, key, value);
-}
-
-export function getJsonItem(file: string, key: string) {
-  return fileIt.getJsonValue(file, key);
-}
-
-export function getFileDataAsJson(file: string) {
-  try {
-    return fileIt.readJsonFileSync(file);
-  } catch (e) {
-    return null;
+  let json: any = getFileDataAsJson(file);
+  if (!json) {
+    json = {};
   }
+  json[key] = value;
+  storeJsonData(file, json);
 }
 
-export function getFileDataArray(file: string) {
-  return fileIt.readJsonArraySync(file);
+export function getFileDataAsJson(filePath: string): any {
+  const content: string = fs.readFileSync(filePath, { encoding: 'utf8' });
+  try {
+    return JSON.parse(content);
+  } catch (e: any) {
+    logIt(`Unable to read session info: ${e.message}`, true);
+  }
+  return null;
 }
 
-export function appendJsonData(file: string, data: any) {
-  fileIt.appendJsonFileSync(file, data);
+export function storeJsonData(filePath: string, json: any) {
+  try {
+    const content: string = JSON.stringify(json);
+    fs.writeFileSync(filePath, content, { encoding: 'utf8' });
+  } catch (e: any) {
+    logIt(`Unable to write session info: ${e.message}`, true);
+  }
 }
