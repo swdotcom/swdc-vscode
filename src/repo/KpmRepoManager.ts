c986a6d238944c060c8d3a431ee01afef851c997
Mason McLead <mason@software.com>
2023-11-27 15:11:21 -0800

    correcting comments

--- src/repo/KpmRepoManager.ts
+++ src/repo/KpmRepoManager.ts
@@ -25,11 +25,12 @@ export async function getResourceInfo(projectDir: string) {
   resourceInfo = {};
 
   const branch = execCmd('git symbolic-ref --short HEAD', projectDir);
+  // returns something like: ['origin\thttps://github.com/swdotcom/swdc-vscode.git (fetch)', 'origin\thttps://github.com/swdotcom/swdc-vscode.git (push)']
   const remotes = execCmd('git remote -v', projectDir, true);
   // find a line that starts with 'origin'
   const origin_remote = remotes.find((line: string) => line.startsWith('origin\t'));
   const remote_name = origin_remote ? 'origin' : remotes[0].split('\t')[0];
-  // returns something like: origin\thttps://github.com/swdotcom/swdc-vscode.git (fetch)'
+
   const identifier = execCmd(`git remote get-url ${remote_name}`, projectDir);
   let email = execCmd('git config user.email', projectDir);
   const tag = execCmd('git describe --all', projectDir);
