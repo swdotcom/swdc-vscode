8c207bc7e49a11a1f42775915e21211d23f26762
Bo Jacobson <bo@software.com>
2024-03-13 13:52:07 -0700

    remove unused slack methods

--- src/managers/SlackManager.ts
+++ src/managers/SlackManager.ts
@@ -1,66 +1,17 @@
-import {commands, window} from 'vscode';
-import {SIGN_UP_LABEL} from '../Constants';
+import { commands, window } from 'vscode';
+import { SIGN_UP_LABEL } from '../Constants';
 import {
   isActiveIntegration,
   setItem
 } from '../Util';
-import {showQuickPick} from '../menu/MenuManager';
-import { getCachedSlackIntegrations, getUser } from '../DataController';
+import { getCachedSlackIntegrations } from '../DataController';
 
-// -------------------------------------------
-// - public methods
-// -------------------------------------------
-
-// get saved slack integrations
 export async function getSlackWorkspaces() {
   return (await getCachedSlackIntegrations()).filter((n: any) => isActiveIntegration('slack', n));
 }
 
 export async function hasSlackWorkspaces() {
   return !!(await getCachedSlackIntegrations()).length;
-}
-
-// -------------------------------------------
-// - private methods
-// -------------------------------------------
-
-async function showSlackWorkspaceSelection() {
-  let menuOptions: any = {
-    items: [],
-    placeholder: `Select a Slack workspace`,
-  };
-
-  (await getSlackWorkspaces()).forEach((integration: any) => {
-    menuOptions.items.push({
-      label: integration.team_domain,
-      value: integration.team_domain,
-    });
-  });
-
-  menuOptions.items.push({
-    label: 'Connect a Slack workspace',
-    command: 'musictime.connectSlack',
-  });
-
-  const pick = await showQuickPick(menuOptions);
-  if (pick) {
-    if (pick.value) {
-      return pick.value;
-    } else if (pick.command) {
-      commands.executeCommand(pick.command);
-      return null;
-    }
-  }
-
-  return null;
-}
-
-/**
- * Remove an integration from the local copy
- * @param auth_id
- */
-function removeSlackIntegration(auth_id: string) {
-  getUser();
 }
 
 export function showModalSignupPrompt(msg: string) {
@@ -105,22 +56,22 @@ export async function checkSlackConnectionForFlowMode() {
   if (!(await hasSlackWorkspaces())) {
     const selection = await window.showInformationMessage(
       "Slack isn't connected",
-      {modal: true},
+      { modal: true },
       ...['Continue anyway', 'Connect Slack']
     );
     if (!selection) {
       // the user selected "cancel"
-      return {continue: false, useSlackSettings: true};
+      return { continue: false, useSlackSettings: true };
     } else if (selection === 'Continue anyway') {
       // slack is not connected, but continue. set useSlackSettings to FALSE
       // set continue to TRUE
       setItem('vscode_CtskipSlackConnect', true);
-      return {continue: true, useSlackSettings: false};
+      return { continue: true, useSlackSettings: false };
     } else {
       // connect was selected
       commands.executeCommand('codetime.manageSlackConnection');
-      return {continue: false, useSlackSettings: true};
+      return { continue: false, useSlackSettings: true };
     }
   }
-  return {continue: true, useSlackSettings: true};
+  return { continue: true, useSlackSettings: true };
 }
