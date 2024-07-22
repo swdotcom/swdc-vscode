177ff5d095f8017b95cb9b66195a0e6d24151b6f
xavluiz <35306917+xavluiz@users.noreply.github.com>
2024-07-21 17:00:15 -0700

    allow unfocus event when window is primary

--- src/managers/ChangeStateManager.ts
+++ src/managers/ChangeStateManager.ts
@@ -2,7 +2,7 @@ import {commands, Disposable, window, workspace} from 'vscode';
 import {TrackerManager} from './TrackerManager';
 import {EditorFlow, EditorType, FlowEventType, ProjectChangeInfo, VSCodeInterface} from '@swdotcom/editor-flow';
 import {configureSettings, showingConfigureSettingsPanel} from './ConfigManager';
-import {getWorkspaceName, setItem} from '../Util';
+import {getWorkspaceName, isPrimaryWindow, setItem} from '../Util';
 import { checkWebsocketConnection } from '../websockets';
 
 export class ChangeStateManager {
@@ -70,6 +70,9 @@ export class ChangeStateManager {
       setItem('vscode_primary_window', getWorkspaceName());
       // check if the websocket connection is stale
       checkWebsocketConnection();
+    } else if (isPrimaryWindow()) {
+      // primary editor window is unfocused
+      this.tracker.trackEditorAction('editor', 'unfocus');
     }
   }
 
