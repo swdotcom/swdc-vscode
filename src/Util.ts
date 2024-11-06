c3785c37ec9c8dc8969aae75eb4502d91e53a00f
xavluiz <35306917+xavluiz@users.noreply.github.com>
2024-11-06 10:41:04 -0800

    revert open npm upgrade and remove unused exports

--- src/Util.ts
+++ src/Util.ts
@@ -16,7 +16,7 @@ import {getBooleanJsonItem, getJsonItem, setJsonItem, storeJsonData} from './man
 import { formatISO } from 'date-fns';
 import { initializeWebsockets, websocketAlive } from './websockets';
 
-import open from 'open';
+const open = require('open');
 import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
